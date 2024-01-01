a1f9156202be0adac78518b2d9f523f5be03a1b5
Claudiu Schwartz <claudiu.schwartz@gmail.com>
2024-01-01 19:55:31 +0100

    PostProcess Highlight --End of Section 1--

--- Source/Aura/Private/Character/AuraEnemy.cpp
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -2,13 +2,27 @@
 
 
 #include "Character/AuraEnemy.h"
+#include "Aura/Aura.h"
+
+
+AAuraEnemy::AAuraEnemy()
+{
+	GetMesh()->SetCollisionResponseToChannel(ECC_Visibility, ECR_Block);
+	GetMesh()->CustomDepthStencilValue = CUSTOM_DEPTH_RED;
+
+	Weapon->SetCollisionResponseToChannel(ECC_Visibility, ECR_Block);
+	Weapon->CustomDepthStencilValue = CUSTOM_DEPTH_RED;
+	
+}
 
 void AAuraEnemy::HighlightActor()
 {
-	bHighlighted = true;
+	GetMesh()->SetRenderCustomDepth(true);
+	Weapon->SetRenderCustomDepth(true);
 }
 
 void AAuraEnemy::UnHighlightActor()
 {
-	bHighlighted = false;
+	GetMesh()->SetRenderCustomDepth(false);
+	Weapon->SetRenderCustomDepth(false);
 }
