30e9899bf6dde22882c1652d5fb837b4d2ffe2ae
Claudiu Schwartz <claudiu.schwartz@gmail.com>
2024-01-01 15:03:00 +0100

    BaseCharacterClass

---
+++ Source/Aura/Aura.cpp
@@ -0,0 +1,6 @@
+// Copyright Predict Edumedia
+
+#include "Aura.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Aura, "Aura" );
---
+++ Source/Aura/Aura.h
@@ -0,0 +1,6 @@
+// Copyright Predict Edumedia
+
+#pragma once
+
+#include "CoreMinimal.h"
+
