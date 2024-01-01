2e8877593b2b1f521c72a900aa9cd0ac48701e08
Claudiu Schwartz <claudiu.schwartz@gmail.com>
2024-01-01 17:38:33 +0100

    AuraPlayerController

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -2,3 +2,35 @@
 
 
 #include "Player/AuraPlayerController.h"
+#include "EnhancedInputSubsystems.h"
+
+AAuraPlayerController::AAuraPlayerController()
+{
+	bReplicates = true;
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	check(AuraContext); //Checks if AuraContext (IMC) is set. //This is an "assert" It crashes game if the pointer isn't set.//
+
+	//Subsystem
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check (Subsystem);
+	Subsystem->AddMappingContext(AuraContext, 0);
+	//
+
+	//Cursor
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+	//
+
+	//InputMode
+	FInputModeGameAndUI InputModeData;
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	InputModeData.SetHideCursorDuringCapture(false);
+	SetInputMode(InputModeData);
+	//
+	
+}
