a0861d4708e10b15d72079d8965d9816870849dc
Claudiu Schwartz <claudiu.schwartz@gmail.com>
2024-01-28 16:05:03 +0100

    Retrieving rows from data table (+lambda InFunctor)

--- Source/Aura/Private/UI/OverlayWidgetController.cpp
+++ Source/Aura/Private/UI/OverlayWidgetController.cpp
@@ -34,13 +34,16 @@ void UOverlayWidgetController::BindCallbacksToDependencies()
 		AuraAttributeSet->GetMaxManaAttribute()).AddUObject(this, &UOverlayWidgetController::MaxManaChanged);
 	
 	Cast<UAuraAbilitySystemComponent>(AbilitySystemComponent)->EffectAssetTags.AddLambda(
-		[] (const FGameplayTagContainer& AssetTags)
+		[this] (const FGameplayTagContainer& AssetTags)
 		// Bind with a LAMBDA FUNCTION .AddLambda( [] () {} ) // the sequence of [] () {} is the signature for a lambda.
 		{
 			for (const FGameplayTag& Tag : AssetTags)
 			{
 				const FString Msg = FString :: Printf(TEXT("GE Tag: %s"), *Tag.ToString());
 				GEngine->AddOnScreenDebugMessage(-1, 8.f, FColor:: Green, Msg);
+
+				FUIWidgetRow* Row = GetDataTableRowByTag<FUIWidgetRow>(MessageWidgetDataTable, Tag);
+				
 			}
 		}
 		);
